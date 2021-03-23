244e1592afb642bc1567ad654442275180f1b4ca
MogammadShaqeelless16 <shaqeelless4@gmail.com>
2021-03-22 22:19:30 -0700

    Updates to the login page and logos and List view menu

---
+++ ECDFind/ECDFind/Views/ECDListPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using ECDFind.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace ECDFind.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ECDListPage : ContentPage
+    {
+        public ECDListPage()
+        {
+            InitializeComponent();
+            BindingContext = new ECDList_ViewModel();
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+
+        }
+    }
+}
